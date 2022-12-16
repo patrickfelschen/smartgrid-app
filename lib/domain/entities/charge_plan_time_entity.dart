0b90fc809e0399f98fc27c319db38a073d51eb66
Patrick Felschen <74025056+patrickfelschen@users.noreply.github.com>
2022-12-16 12:23:55 +0100

    variables

--- lib/domain/entities/charge_plan_time_entity.dart
+++ lib/domain/entities/charge_plan_time_entity.dart
@@ -1,1 +1,14 @@
-class ChargePlanTimeEntity {}
+class ChargePlanTimeEntity {
+  final DateTime time;
+  final double power;
+  // grams of carbon dioxide equivalent per kilowatt-hour of electricity
+  final double gCO2eqPerkWh;
+  final double powerGridCapacity;
+
+  ChargePlanTimeEntity({
+    required this.time,
+    required this.power,
+    required this.gCO2eqPerkWh,
+    required this.powerGridCapacity,
+  });
+}
