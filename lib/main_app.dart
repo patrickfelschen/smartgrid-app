8b225387a189c749f553545c03a55d5262ef40ec
Patrick Felschen <74025056+patrickfelschen@users.noreply.github.com>
2022-12-22 13:35:59 +0100

    flutter_localizations

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smartgrid/app/pages/authentication/customer_sign_in_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -8,6 +9,15 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), // English, no country code
+        Locale('de', ''), // German, no country code
+      ],
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
