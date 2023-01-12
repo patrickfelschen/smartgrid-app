3f0e7d969e468d2b19d31d03a8c33aef731646c8
Julian Voß <julian.voss97@outlook.de>
2023-01-12 15:12:51 +0100

    api path

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
   static const authStorageKey = 'auth3';
-  static const smartGridApiBaseUrl = "api.smartgrid.de";
-  static const testMode = true;
+  static const smartGridApiBaseUrl = "131.173.127.222";
+  static const testMode = false;
 }
